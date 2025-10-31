ac7f5126c033202898793da006bbd17ea73c10a2
Vikrant Singh <vikrantsingh1012@gmail.com>
2025-10-31 15:17:01 +0530

    added all files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "Hello World!"
+
+@app.route("/welcome")
+def welcome():
+   return "Welcome to githubactions!"
+
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=8080)
